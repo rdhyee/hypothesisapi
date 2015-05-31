d2e9ec61a87376e9f41eb479a8e2f6b910fd78a1
bbarker <beb82@cornell.edu>
2015-05-31 10:52:15 -0400

    added tentative create function
    very minor
    
    minor improvements

--- hypothesisapi/__init__.py
+++ hypothesisapi/__init__.py
@@ -35,6 +35,63 @@ class API(object):
         self.password = password
         self.csrf_token = None
         self.token = None
+
+    def create (self, payload):
+        
+        user = self.username
+        user_acct = "acct:{user}@hypothes.is".format(user=user)
+        
+        payload_out = payload.copy()
+        payload_out["user"] = user_acct
+        if not "uri" in payload:
+            return None
+        if not "permissions" in payload:
+            perms = {
+                "read"  : ["group:__world__"],
+                "update": ["acct:{user}@hypothes.is".format(user=user)],
+                "delete": ["acct:{user}@hypothes.is".format(user=user)],
+                "admin" : ["acct:{user}@hypothes.is".format(user=user)]
+                }
+            payload_out["permissions"] = perms
+        if not "document" in payload:
+            doc = {}
+            payload_out["document"] = doc
+        if not "text" in payload:
+            payload_out["text"] = None            
+        if not "tags" in payload:
+            payload_out["tags"] = None
+        if not "target" in payload: 
+            target = [
+                {
+                "selector": 
+                    [
+                        {
+                        "start": None,
+                        "end": None,
+                        "type": "TextPositionSelector"
+                        }, 
+                        {
+                        "type": "TextQuoteSelector", 
+                        "prefix": None,
+                        "exact": None,
+                        "suffix": None
+                        },
+                    ]
+                }
+            ]
+            payload_out["target"] = target
+        data = json.dumps(payload_out)
+        headers = {"content-type": "application/json;charset=UTF-8",
+                   "X-Annotator-Auth-Token": self.token, 
+                   "x-csrf-token": self.csrf_token
+                   }
+        r = requests.post(self.api_url + "/annotations", headers = headers, data = data)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            print("Failed\n")
+            print(r.text)
+            return None
         
     def login(self):
         
@@ -60,7 +117,6 @@ class API(object):
         
  
         self.token =  r.content
-        
     
     def search_id (self, _id):
         
