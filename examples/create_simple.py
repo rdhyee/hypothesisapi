1cde50e0f7df6ba9cfb412a8945133a350c40890
bbarker <beb82@cornell.edu>
2015-06-02 11:57:20 -0400

    added create example

---
+++ examples/create_simple.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+import argparse
+import requests
+
+from hypothesisapi import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', dest='user', action='store',
+                    help='hypothes.is user')
+
+parser.add_argument('-p', dest='password', action='store',
+                    help='hypothes.is password')
+
+parser.add_argument('-l', dest='test_uri', action='store',
+                    help='url to annotate')
+
+args = parser.parse_args()
+
+# Set up Hypothes.is config
+H = API(args.user, args.password)
+H.login()
+
+# Now test create on test_uri
+payload = {
+    "uri"  : args.test_uri,
+    "text" : "testing create in hypothes.is API"
+    }
+    
+r = H.create(payload)
+
+if (r is not None):
+    print(r)
+
+
